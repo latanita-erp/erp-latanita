4d411f30eb6a7e90e87158c9c189611a6e66117a
latanita-erp <claudiorubenboveda@gmail.com>
2026-05-07 08:41:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
-password = urllib.parse.quote_plus('Xeneize1198!')
+password = urllib.parse.quote_plus('Xeneize1198$')
 DB_URI = f"postgresql+pg8000://postgres.juzwfwgonamxyuvoxgbj:{password}@aws-1-sa-east-1.pooler.supabase.com:6543/postgres"
 
 
